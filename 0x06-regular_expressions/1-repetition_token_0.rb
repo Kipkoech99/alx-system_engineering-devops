e7a5c7e9d264876c3dbd3603c0f1001790748494
Kipkoech99 <kipkoechevans99.com>
2023-10-03 09:01:13 -0700

    New file and few changes

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -2,4 +2,4 @@
 # Repetition Token #1
 # Find the regular expression that will match the above cases
 
-puts ARGV[0].scan(/hbt{2, 5}n/).join
+puts ARGV[0].scan(/hbt{2,5}n/).join
--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -2,4 +2,4 @@
 #Repetition Token #3
 #Find the regular expression that will match the above cases
 
-puts ARGV[0].scan(/hbt?n/).join
+puts ARGV[0].scan(/hbt*n/).join
